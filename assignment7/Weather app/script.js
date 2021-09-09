f9d9dac1eddbcaa280c5177c9c0eff2fc3ee772b
Shubharil Bhardwaj <76536981+Shubharil10@users.noreply.github.com>
2021-09-09 14:17:32 +0530

    Update script.js

--- assignment7/Weather app/script.js
+++ assignment7/Weather app/script.js
@@ -1,7 +1,7 @@
 let weather = {
     "apiKey": "3ef3118bb68f4b082639a1147a7b6d51",
     fetchWeather: function (city) {
-        fetch("http://api.openweathermap.org/data/2.5/weather?q=" 
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
         + city
         + "&units=metric&appid="
         + this.apiKey
